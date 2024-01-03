4ba6cea39c949e7d95772665628fd4553a17933d
choeyunseo <choeyunseo4060@naver.com>
2024-01-04 01:23:29 +0900

    Add 1-6.py

--- coding_interview/1-6.py
+++ coding_interview/1-6.py
@@ -10,19 +10,29 @@ def compress(s: str) -> str:
     for i in range(len(s) - 1):
         if s[i] == s[i + 1]:
             cnt += 1
-        else:
+            # 끝에 문자 처리
+            if i == len(s) - 2:
+                charArray.append(s[i] + str(cnt))
+        elif s[i] != s[i + 1]:
             charArray.append(s[i] + str(cnt))
             cnt = 1
+            if i == len(s) - 2:
+                charArray.append(s[i + 1] + str(cnt))
+                
+    # 리스트 -> 문자열
+    charArray = ''.join(charArray)
 
-
-
+    if len(charArray) > len(s):
+        return s
+    else:
+        return charArray
 
 if __name__ == '__main__':
     # Write your test cases here
     testcase1 = "aabccccaaa"
-    testcase2 = "ababb"
+    testcase2 = "ababb" # < "a1b1a1b2"
     testcase3 = "zzxxccvvbb" 
 
     assert compress(testcase1) == "a2b1c4a3"
-    assert compress(testcase2) == "b1"
+    assert compress(testcase2) == "ababb"
     assert compress(testcase3) == "z2x2c2v2b2"
